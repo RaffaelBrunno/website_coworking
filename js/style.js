0a14aa348c902c10eaa4e9ac32ee44b2d34cca1e
RaffaelBrunno <167913417+RaffaelBrunno@users.noreply.github.com>
2024-05-16 20:50:21 -0300

    Add files via upload

---
+++ js/style.js
@@ -0,0 +1,14 @@
+
+
+function show(){
+
+    var menu = document.querySelector(".list-mobile");
+    if (menu.classList.contains('none')){
+
+    }else{
+        menuMobile.classList.add('none');
+        alert('teste')
+    }
+}
+
+
